edde648de5e2986a31abc77f1356a6e9aa2aaf1b
ryantrappy <ryantrapp@live.com>
2019-09-16 21:26:34 -0500

    Added introduction and title to be pulled from league Config

--- fantasy-power-rankings/src/app/app.component.ts
+++ fantasy-power-rankings/src/app/app.component.ts
@@ -41,8 +41,44 @@ export class AppComponent implements OnInit{
       "managerName": "Joe Loser2"
     }
   ];
-  previousWeekRanking: Array<TeamRanking> = [];
-  leagueConfig: LeagueConfig;
+  previousWeekRanking: Array<TeamRanking> = [
+    {
+      "teamName": "Test Winner",
+      "description": "They did their best this week but sometimes that just is good enough.",
+      wins: 1,
+      loss: 0,
+      ties: 0,
+      "managerName": "Joe Winner"
+    },
+    {
+      "teamName": "Test Loser",
+      "description": "They did their best this week but sometimes that just is not good enough.",
+      wins: 0,
+      loss: 1,
+      ties: 0,
+      "managerName": "Joe Loser2"
+    },
+    {
+      "teamName": "Test Loser",
+      "description": "They did their best this week but sometimes that just is not good enough.",
+      wins: 0,
+      loss: 1,
+      ties: 0,
+      "managerName": "Joe Loser"
+    },
+    {
+      "teamName": "Test Loser",
+      "description": "They did their best this week but sometimes that just is not good enough.",
+      wins: 0,
+      loss: 1,
+      ties: 0,
+      "managerName": "Joe Loser1"
+    }
+  ];
+  leagueConfig: LeagueConfig = {
+    "rankingsTitle": "Rankings Title",
+    "introduction": "This is introduction text if the user wants to preface the rankings with some text. The text can be an introduction to the year or anything else."
+  };
 
   ngOnInit(): void {
     this.getRankingImage();
